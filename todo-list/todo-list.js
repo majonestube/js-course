ac0811205bbd7f7d36eb122d2bc346046fc6cd4f
majonestube <martine.vold@hotmail.com>
2025-08-23 11:41:26 +0200

    Added date to todo list

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,8 +1,45 @@
+
+
 const todoList = [];
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+    const html = `
+    <p>
+      ${name} ${dueDate}
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();">
+      Delete</button>
+    </p>`;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  todoList.push(name);
+
+  const dateInputElement = document.querySelector('.js-due-date-input')
+  const dueDate = dateInputElement.value;
+
+  if (name != '') {
+  todoList.push({
+    name,
+    dueDate
+    });
+  }
   console.log(todoList);
+
+  inputElement.value = '';
+
+  renderTodoList();
 };
